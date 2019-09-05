cdb0bdfd3fb27f98aa11e5d050226be348b733bd
Karen Tserunyan <karen.tserunyan@gmail.com>
2019-09-05 17:22:08 +0400

    page models and WebDriverSingleton

---
+++ src/main/java/com/epam/atmp/pages/app/ConfigPage.java
@@ -0,0 +1,27 @@
+package com.epam.atmp.pages.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import utils.WebDriverSingleton;
+
+public class ConfigPage {
+
+    protected WebDriver driver;
+
+    By titleText = By.xpath("//");
+
+    public ConfigPage(){
+
+        this.driver = WebDriverSingleton.getWebDriverInstance();
+
+    }
+
+
+    public String getPageTitle(){
+
+        return    driver.findElement(titleText).getText();
+
+    }
+
+
+}
---
+++ src/main/java/utils/WebDriverSingleton.java
@@ -0,0 +1,40 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSingleton {
+    private static WebDriver instance;
+
+    private WebDriverSingleton() {
+    }
+
+    public static WebDriver getWebDriverInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        return instance = init();
+    }
+
+    private static WebDriver init() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void kill() {
+        if (instance != null) {
+            try {
+                instance.quit();
+            } catch (Exception e) {
+                System.out.println("Cannot kill browser");
+            } finally {
+                instance = null;
+            }
+        }
+    }
+}
